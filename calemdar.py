b21336726d2e2a3e79ff22c2e1da86dd96512833
Flurt <umidrukhiddinov70@gmail.com>
2022-11-21 17:56:20 +0500

    add test.py

---
+++ calemdar.py
@@ -0,0 +1,13 @@
+# Python program to display calendar of
+# given month of the year
+
+# import module
+import calendar
+
+yy = 2017
+mm = 12
+
+# display the calendar
+a = calendar.calendar(yy,)
+
+print(a)
